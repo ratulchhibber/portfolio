153330f2f33b75408279d9eb6e2f72739fa5f9e3
Ratul Chhibber <ratulchhibber@gmail.com>
2023-02-27 23:14:03 +0530

    * Adding projects section

--- src/index.js
+++ src/index.js
@@ -14,3 +14,28 @@ document.ontouchmove = (e) => handleMove(e.touches[0]);
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+$(function () {
+  $(".box").on("click touchstart", function (e) {
+    window.event.stopPropagation();
+    $(".box").not(this).removeClass("touched");
+    $(this).toggleClass("touched").removeClass("hovered");
+  });
+  $(".box").on("mouseenter", function (e) {
+    $(this).addClass("hovered");
+  });
+  $(".box").on("mouseleave", function (e) {
+    $(this).removeClass("hovered");
+  });
+});
+
+$(document).on("click touchstart touchend", function () {
+  console.log("event");
+
+  $(".box").removeClass("touched");
+});
+
+/*
+script based on this pen
+https://codepen.io/lukebrooker/pen/aCiol
+*/
