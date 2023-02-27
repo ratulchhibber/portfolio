8f8dca4ccb2189628ee9910d267dc2bd6d179640
Ratul Chhibber <ratulchhibber@gmail.com>
2023-02-27 21:38:23 +0530

    * Adding Sliding header

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
+const left = document.getElementById("left-side");
+
+const handleMove = (e) => {
+  left.style.width = `${(e.clientX / window.innerWidth) * 100}%`;
+};
+
+document.onmousemove = (e) => handleMove(e);
+
+document.ontouchmove = (e) => handleMove(e.touches[0]);
+
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
